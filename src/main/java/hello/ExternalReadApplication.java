60ef92b2faccfac1d54b8a73ee212668d1a09c50
bigbulgogiburger <dohoon321@gmail.com>
2023-07-15 13:58:15 +0900

    added

--- src/main/java/hello/ExternalReadApplication.java
+++ src/main/java/hello/ExternalReadApplication.java
@@ -1,8 +1,6 @@
 package hello;
 
-import hello.config.MyDataSourceConfigV1;
-import hello.config.MyDataSourceEnvConfig;
-import hello.config.MyDataSourceValueConfig;
+import hello.config.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -10,9 +8,10 @@ import org.springframework.context.annotation.Import;
 
 //@Import(MyDataSourceEnvConfig.class)
 //@Import(MyDataSourceValueConfig.class)
-@Import(MyDataSourceConfigV1.class)
-@ConfigurationPropertiesScan({"hello"})
-@SpringBootApplication(scanBasePackages = "hello.datasource")
+//@Import(MyDataSourceConfigV1.class)
+@Import(MyDataSourceConfigV3.class)
+@ConfigurationPropertiesScan
+@SpringBootApplication(scanBasePackages = {"hello.datasource","hello.pay"})
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
